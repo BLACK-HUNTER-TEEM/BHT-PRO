2a2d216ff742ec5d46a31f4ef65969b5f3f38865
BLACK-HUNTER-TEAM <105848361+BLACK-HUNTER-TEEM@users.noreply.github.com>
2022-07-21 22:01:36 +0600

    Update jahied-x2.py

--- jahied-x2.py
+++ jahied-x2.py
@@ -97,7 +97,7 @@ os.system('termux-setup-storage')
 def helpnote():
 	print("%s [=>] FOLLOW ME ON  FB TU KNOW ABOUT UPDATES  :)"%(G))
 	#yahan nichy httsp sy hata kr apna github aproval link dalna
-	r1=requests.get("https://github.com/BLACK-HUNTER-TEEM/BHT-PRO-v2/main/approval.txt"])
+	r1=requests.get("https://github.com/BLACK-HUNTER-TEEM/python jahied-x2.py/main/approval.txt"])
 	if key1 in r1:
     #yahan apni facebook id link dalna
 	exit(" [=>] FACEBOOK :  https://www.facebook.com/ROOT.OFF.JAHIED")
